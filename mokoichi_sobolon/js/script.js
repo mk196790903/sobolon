0ff91ee82b9ee02f0a8d5404daea4f6137a62bce
mokoichi <134169342+mk196790903@users.noreply.github.com>
2026-03-30 19:13:23 +0900

    Add files via upload

---
+++ mokoichi_sobolon/js/script.js
@@ -0,0 +1,6 @@
+document.querySelector("#js-drawer-button").addEventListener("click", function(e)  {
+  e.preventDefault();
+
+  document.querySelector("#js-drawer-button").classList.toggle("is-checked");
+  document.querySelector("#js-drawer-content").classList.toggle("is-checked");
+});
